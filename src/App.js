e355ce6311c19c325c25fb6929c85826b5c6a6bd
sayadaazami <sayadaazami@gmail.com>
2020-05-21 22:57:29 +0430

    003

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import React from "react";
-import TestComponent from "./TestComponent";
 
 export default class App extends React.Component {
   state = {
     value: 0,
+    otherValue: this.props.name,
   };
 
-  handleChildPropChange = (data) => {
-    console.log(data);
-    // this.state = data; // naaaaaaaaaaaaaaaaaaaaaa!
+  incValue = () => this.setState((state) => ({ value: state.value + 1 }));
 
-    this.setState({
-      value: data,
-    });
-  };
+  resetValue = () => this.setState({ value: 0 });
+
+  constructor(props) {
+    super(props);
+
+    console.log("constructor");
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
 
   render() {
     return (
       <section>
-        <TestComponent onChange={this.handleChildPropChange}>
-          man TestComponent am
-        </TestComponent>
+        <header>
+          <h1>{this.props.name}</h1>
+        </header>
 
-        <div>{this.state.value}</div>
+        <div>value: {this.state.value}</div>
+        <div>otherValue: {this.state.otherValue}</div>
 
-        <button onClick={() => this.setState({ value: this.state.value + 1 })}>
-          myButton
-        </button>
+        <button onClick={this.incValue}>inc</button>
+        <button onClick={this.resetValue}>reset</button>
+        <button onClick={() => this.forceUpdate()}>force</button>
       </section>
     );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    //کل api ها اینجا صدا زده میشه
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("componentWillReceiveProps", nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextStates) {
+    console.log("shouldComponentUpdate", nextProps, nextStates);
+
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextStates) {
+    console.log("componentWillUpdate", nextProps, nextStates);
+  }
+
+  componentDidUpdate(prevProps, prevStates) {
+    console.log("componentDidUpdate", prevProps, prevStates);
+    //کل api ها اینجا صدا زده میشه
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
   }
 }
